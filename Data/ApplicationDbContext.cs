24a9700fd0facc7cd3da48471936462fac7aec92
jackfperryjr <jack.franklin.perryjr@gmail.com>
2020-03-09 07:19:49 -0400

    Changed start url in manifest.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace Terra.Data
         public DbSet<Terra.Models.Game> Games { get; set; }  
         public DbSet<Terra.Models.Picture> Pictures { get; set; }  
         public DbSet<Terra.Models.Stat> Stats { get; set; }  
+        public DbSet<Terra.Models.DatingProfile> DatingProfile { get; set; }
+        public DbSet<Terra.Models.DatingResponse> DatingResponses { get; set; }
         
         // protected override void OnModelCreating(ModelBuilder builder)
         // {
